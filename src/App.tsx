99d82fb99977b9d781fb398e3eadd8a3527dfd42
Fortino Hogan Hadiprodjo <fortino.hadiprodjo@binus.edu>
2024-09-21 21:25:23 +0700

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './views/home-page/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to rasdasdaseload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='*' element={<HomePage />} />
+    </Routes>
   );
 }
 
