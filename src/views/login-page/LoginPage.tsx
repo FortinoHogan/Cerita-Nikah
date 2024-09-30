870fd9ed117404f7687cb50f24789894a6370843
Fortino Hogan <fortinohogan@gmail.com>
2024-09-30 15:55:00 +0700

    feat: add login page

---
+++ src/views/login-page/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginPageLeft from '../../layouts/login-page-left/LoginPageLeft'
+
+const LoginPage = () => {
+  return (
+    <div className='grid grid-cols-2'>
+        <LoginPageLeft />
+        <div className=''>test</div>
+    </div>
+  )
+}
+
+export default LoginPage
