c9652892160f482dfed7ae141edee914a1816271
Steven <steven@binus.edu>
2024-10-01 17:30:11 +0700

    test: handling subdomain routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './views/home-page/HomePage';
-import LoginPage from './views/login-page/LoginPage';
-import RegisterPage from './views/register-page/RegisterPage';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./views/home-page/HomePage";
+import LoginPage from "./views/login-page/LoginPage";
+import RegisterPage from "./views/register-page/RegisterPage";
+import { template } from "./libs/template";
+import TemplatePersonalizedPage from "./views/template-personalized-page/TemplatePersonalizedPage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path='*' element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-    </Routes>
-  );
+  const isSubDomain = window.location.host === template.domain;
+
+  if (isSubDomain && template.isPay) {
+    return (
+      <Routes>
+        <Route path="*" element={<TemplatePersonalizedPage />} />
+      </Routes>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="*" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/template-personalized-page/TemplatePersonalizedPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TemplatePersonalizedPage = () => {
+  return (
+    <div>Hello from SubDomain</div>
+  )
+}
+
+export default TemplatePersonalizedPage
