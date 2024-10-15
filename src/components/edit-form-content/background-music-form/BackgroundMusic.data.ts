4b06fabf6aff5c5da201df410cd2845def931851
FortinoHogan <fortinohogan@gmail.com>
2024-10-15 15:43:29 +0700

    fix: adjust background music stop

--- src/components/edit-form-content/background-music-form/BackgroundMusic.data.ts
+++ src/components/edit-form-content/background-music-form/BackgroundMusic.data.ts
@@ -1,10 +1,14 @@
 export const backgroundMusicData = [
-    {
-        name: "Kisah Kasih Di Sekolah - Chrisye",
-        path: "assets/audio/Kisah Kasih Di Sekolah - Chrisye.mp3",
-    },
-    {
-        name: "Aku Cinta Dia - Chrisye",
-        path: "assets/audio/Aku Cinta Dia - Chrisye.mp3",
-    }
-]
+  {
+    name: "Kisah Kasih Di Sekolah - Chrisye",
+    path: "assets/audio/Kisah Kasih Di Sekolah - Chrisye.mp3",
+  },
+  {
+    name: "Aku Cinta Dia - Chrisye",
+    path: "assets/audio/Aku Cinta Dia - Chrisye.mp3",
+  },
+  {
+    name: "Cantik - Kahitna",
+    path: "assets/audio/Cantik - Kahitna.mp3",
+  },
+];
