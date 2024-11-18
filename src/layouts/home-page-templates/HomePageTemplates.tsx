b60f0a468b6253a149eec3868b19073884014c6d
FortinoHogan <fortinohogan@gmail.com>
2024-11-18 08:30:29 +0700

    style: adjust width homepage templates

--- src/layouts/home-page-templates/HomePageTemplates.tsx
+++ src/layouts/home-page-templates/HomePageTemplates.tsx
@@ -6,14 +6,14 @@ const HomePageTemplates = () => {
   return (
     <div className="relative min-h-screen flex flex-col gap-10 items-center justify-center">
       <div className="flex items-center justify-center gap-3">
-        <h1 className="text-6xl max-sm:text-4xl font-semibold text-custom-pink">
+        <h1 className="text-6xl max-sm:text-4xl font-semibold text-custom-pink break-words">
           Template Design
         </h1>
         <span className="min-h-[5px] mt-3 min-w-[60px] bg-custom-pink max-sm:hidden"></span>
       </div>
       <div className="flex gap-20 max-xl:flex-col contentTemplate">
         {templates.map((template, index) => (
-          <div className="flex flex-col items-center gap-5 w-[30rem] rounded-xl transition-all duration-300 hover:scale-105" key={index}>
+          <div className="flex flex-col items-center gap-5 w-fit rounded-xl transition-all duration-300 hover:scale-105" key={index}>
             <img
               className="w-full h-96 max-sm:max-w-[20rem] max-sm:max-h-[20rem] object-cover rounded-xl shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
               src={template.image}
